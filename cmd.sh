a627f15c021e5c5281bb33394d0e52bc1dbcecc1
Kresten Laust <krestenlaust@gmail.com>
2025-01-25 21:17:20 +0100

    Update cmd.sh with new command

--- cmd.sh
+++ cmd.sh
@@ -1,2 +1,7 @@
 ansible-galaxy install -r requirements.yml
-ansible-playbook -i inventory.ini playbook.yml --ask-become-pass
+
+# Run playbook, prompt password
+# ansible-playbook -i inventory.ini playbook.yml --ask-become-pass
+
+# Run playbook, without prompting for password
+ansible-playbook -i inventory.ini playbook.yml --extra-vars "ansible_become_pass=123"
